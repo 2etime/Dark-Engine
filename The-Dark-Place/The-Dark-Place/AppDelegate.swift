f1b014a3ff34be4660b52df2a34eb0324bb24e6d
twohyjr <twohyjr@gmail.com>
2018-10-19 19:23:19 -0400

    Created The Dark Place

---
+++ The-Dark-Place/The-Dark-Place/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  The-Dark-Place
+//
+//  Created by Rick Twohy on 10/19/18.
+//  Copyright © 2018 2etime. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ The-Dark-Place/The-Dark-Place/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  The-Dark-Place
+//
+//  Created by Rick Twohy on 10/19/18.
+//  Copyright © 2018 2etime. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
